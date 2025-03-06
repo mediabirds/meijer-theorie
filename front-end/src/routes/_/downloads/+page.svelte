f6114e3306e6deef2c546926c347162c38306cb8
codeit-ninja <richard@codeit.ninja>
2025-03-06 17:57:48 +0100

    Show message if no downloads available

--- front-end/src/routes/_/downloads/+page.svelte
+++ front-end/src/routes/_/downloads/+page.svelte
@@ -5,29 +5,32 @@
 	import FileIcon from 'lucide-svelte/icons/paperclip'
 	import DownloadIcon from 'lucide-svelte/icons/download'
 	import { PUBLIC_DIRECTUS_URL } from '$env/static/public'
+	import { Alert } from '$lib/components/ui/alert'
 
 	const { data } = $props()
-
-	console.log(data.downloads)
 </script>
 
 <Box title={$_('pages.downloads.title')} description={$_('pages.downloads.description')}>
-	<div class="flex flex-col gap-2">
-		{#each data.downloads as download}
-			<span class="flex gap-2">
-				<span class="flex flex-grow items-center gap-2 rounded-lg bg-neutral-100 px-6 py-3">
-					<FileIcon class="text-neutral-400" />
-					{download.filename}
+	{#if data.downloads.length > 0}
+		<div class="flex flex-col gap-2">
+			{#each data.downloads as download}
+				<span class="flex gap-2">
+					<span class="flex flex-grow items-center gap-2 rounded-lg bg-neutral-100 px-6 py-3">
+						<FileIcon class="text-neutral-400" />
+						{download.filename}
+					</span>
+					<Button
+						variant="secondary"
+						href={`${PUBLIC_DIRECTUS_URL}/assets/${download.file}?download`}
+						target="_blank"
+					>
+						<DownloadIcon />
+						{$_('common.download')}
+					</Button>
 				</span>
-				<Button
-					variant="secondary"
-					href={`${PUBLIC_DIRECTUS_URL}/assets/${download.file}?download`}
-					target="_blank"
-				>
-					<DownloadIcon />
-					{$_('common.download')}
-				</Button>
-			</span>
-		{/each}
-	</div>
+			{/each}
+		</div>
+	{:else}
+		<Alert>{$_('pages.downloads.no_downloads')}</Alert>
+	{/if}
 </Box>
